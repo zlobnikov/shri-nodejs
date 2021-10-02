8282fcc4cb247c585c93ee3ec5737023cd388864
zlobnikov <daffodil@list.ru>
2021-10-02 19:56:57 +0300

    fix: change upload return

--- src/server.js
+++ src/server.js
@@ -25,13 +25,13 @@ const upload = multer({ storage: storage })
 app.get('/ping', (req, res) => res.json({ ping: 'pong' }));
 
 app.post('/upload', upload.single('image'), async (req, res) => {
-  const fileName = req.file.filename.split('.')[0];
-  const fileSize = req.file.size;
-  const img = new Image(fileName, fileSize);
+  const id = req.file.filename.split('.')[0];
+  const size = req.file.size;
+  const img = new Image(id, size);
 
   await db.insert(img);
 
-  return res.json(img.toJSON());
+  return res.json({id});
 });
 
 app.listen(PORT, () => {
