7fb66633b12e28a34cb7c09046b60ac316901510
Konstantin Belousov <32342483+kombuchamp@users.noreply.github.com>
2021-10-08 22:04:43 +0300

    review

--- src/utils/fs.js
+++ src/utils/fs.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
 const util = require('util');
 
+Я бы не называл этот модуль fs, вызывает вопросы, почему он назывется так же, как core-модуль
+
+Эти штуки - полезные 👍
 const writeFileAsync = util.promisify(fs.writeFile);
 const unlinkFileAsync = util.promisify(fs.unlink);
 const existsFileAsync = util.promisify(fs.exists);
 
+Все эти утилиты выглядят лишними. Все это можно заинлайнить в код
 module.exports = {
   writeFile: async (path, content) => {
     await writeFileAsync(path, content, { encoding: 'utf-8' });
